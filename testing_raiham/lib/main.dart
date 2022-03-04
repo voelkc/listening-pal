d2c9cb0084a5f94d4cc898d49b5ab84bef2d0fd7
Raihamm <raiham@uw.edu>
2022-03-03 16:27:13 -0800

    calendar lookin better

--- testing_raiham/lib/main.dart
+++ testing_raiham/lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:testing_raiham/pages/appointments.dart';
 // import 'package:onboarding/onboarding.dart';
 import 'pages/onboarding.dart';
 
 import 'package:http/http.dart' as http;
 import 'package:google_fonts/google_fonts.dart';
-
 
 void main() {
   runApp(MyApp());
@@ -17,109 +17,24 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: title,
         theme: ThemeData(
-            scaffoldBackgroundColor: Color(0xffF9F9F9),
-            primaryColor: Color.fromRGBO(149, 212, 216, 1),
-            secondaryHeaderColor: Color(0xff41434D) ,
-            primarySwatch: Colors.blue,
+          scaffoldBackgroundColor: Color(0xffF9F9F9),
+          primaryColor: Color.fromRGBO(149, 212, 216, 1),
+          secondaryHeaderColor: Color(0xff41434D),
+          primarySwatch: Colors.blue,
           textTheme: const TextTheme(
-            headline1: TextStyle(fontSize: 64.0, fontWeight: FontWeight.bold, color:Color(0xff41434D)),
-            headline6: TextStyle(fontSize: 24.0, color:Color(0xff41434D)),
-            bodyText2: TextStyle(fontSize: 18.0, color:Color(0xff41434D)),
-            button: TextStyle(fontSize: 16.0, color:Color(0xff41434D)),
-            bodyText1: TextStyle(fontSize: 18.0, decoration: TextDecoration.underline, color:Color(0xff41434D)), // clickable text!
+            headline1: TextStyle(
+                fontSize: 64.0,
+                fontWeight: FontWeight.bold,
+                color: Color(0xff41434D)),
+            headline6: TextStyle(fontSize: 24.0, color: Color(0xff41434D)),
+            bodyText2: TextStyle(fontSize: 18.0, color: Color(0xff41434D)),
+            button: TextStyle(fontSize: 16.0, color: Color(0xff41434D)),
+            bodyText1: TextStyle(
+                fontSize: 18.0,
+                decoration: TextDecoration.underline,
+                color: Color(0xff41434D)), // clickable text!
           ),
         ),
-        home: OnboardingPage(),
+        home: ApptPage(),
       );
-  // final onboardingPagesList = [
-  //   PageModel(
-  //     widget: Column(
-  //       children: [
-  //         Container(
-  //             padding: EdgeInsets.only(bottom: 45.0),
-  //             child: Image(image: AssetImage('img/initiallog.jpg'))),
-  //         Container(
-  //             width: double.infinity,
-  //             child: Text('Speak to a Listening Pal or Become One',
-  //                 style: pageTitleStyle)),
-  //         Container(
-  //           width: double.infinity,
-  //           child: Text(
-  //             'Became a healthier and happier YOU',
-  //             style: pageInfoStyle,
-  //           ),
-  //         ),
-  //       ],
-  //     ),
-  //   ),
-  //   PageModel(
-  //     widget: Column(
-  //       children: [
-  //         Padding(padding: EdgeInsets.only(bottom: 45.0)),
-  //         Image(image: AssetImage('img/people.jpg')),
-  //         // attribution <a href="https://www.flaticon.com/free-icons/people" title="people icons">People icons created by Freepik - Flaticon</a>
-  //         Text('Share your experiences', style: pageTitleStyle),
-  //         Text(
-  //           'Peer Support can create a path for better mental health',
-  //           style: pageInfoStyle,
-  //         )
-  //       ],
-  //     ),
-  //   ),
-  //   PageModel(
-  //     widget: Column(
-  //       children: [
-  //         Image(image: AssetImage('img/initiallog.jpg')),
-  //         Text('Sign Up Today', style: pageTitleStyle),
-  //         Text(
-  //           'In just a few easy steps you can make a change',
-  //           style: pageInfoStyle,
-  //         ),
-  //       ],
-  //     ),
-  //   ),
-  // ];
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   Color logoColor = const Color.fromRGBO(249, 249, 249, 0);
-  //   return MaterialApp(
-  //     debugShowCheckedModeBanner: false,
-  //     title: 'Listening Pal Test',
-  //     theme: ThemeData(
-  //       scaffoldBackgroundColor: const Color.fromRGBO(249, 249, 249, 0),
-  //       primaryColor: Colors.blue,
-  //       visualDensity: VisualDensity.adaptivePlatformDensity,
-  //     ),
-  //     home: Onboarding(
-  //       proceedButtonStyle: ProceedButtonStyle(
-  //         proceedButtonRoute: (context) {
-  //           return Navigator.pushAndRemoveUntil(
-  //             context,
-  //             MaterialPageRoute(
-  //               builder: (context) => Container(),
-  //             ),
-  //             (route) => false,
-  //           );
-  //         },
-  //       ),
-  //       // isSkippable = true,
-
-  //       pages: onboardingPagesList,
-  //       indicator: Indicator(
-  //         indicatorDesign: IndicatorDesign.line(
-  //           lineDesign: LineDesign(
-  //             lineType: DesignType.line_uniform,
-  //           ),
-  //         ),
-  //       ),
-  //       //-------------Other properties--------------
-  //       //Color background,
-  //       //EdgeInsets pagesContentPadding
-  //       //EdgeInsets titleAndInfoPadding
-  //       //EdgeInsets footerPadding
-  //       //SkipButtonStyle skipButtonStyle
-  //     ),
-  //   );
-  // }
 }
--- testing_raiham/lib/pages/home.dart
+++ testing_raiham/lib/pages/home.dart
@@ -4,156 +4,216 @@ import '../main.dart';
 import './onboarding.dart';
 import './resourcespage.dart';
 import './callpage.dart';
+import './appointments.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
-            backgroundColor:  Theme.of(context).scaffoldBackgroundColor,
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              IconButton(
-                onPressed: () => goToResources(context),
-                icon:  const Icon(
-                Icons.folder,
-                color:  Color(0xff41434D),
-                size: 24.0,
-                semanticLabel: 'Text to announce in accessibility modes',
-              ),),
-
-             const Icon(
-                Icons.settings,
-                color: Color(0xff41434D),
-                size: 30.0,
-              ),
-            ],
-          )
-        ),
+            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                IconButton(
+                  onPressed: () => goToResources(context),
+                  icon: const Icon(
+                    Icons.folder,
+                    color: Color(0xff41434D),
+                    size: 24.0,
+                    semanticLabel: 'Text to announce in accessibility modes',
+                  ),
+                ),
+                const Icon(
+                  Icons.settings,
+                  color: Color(0xff41434D),
+                  size: 30.0,
+                ),
+              ],
+            )),
         body: Center(
-          child: Padding(padding:EdgeInsets.fromLTRB(10, 0, 10, 0), child:
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, crossAxisAlignment: CrossAxisAlignment.end,
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Column(mainAxisAlignment: MainAxisAlignment.start, crossAxisAlignment: CrossAxisAlignment.start, children: [
-                  Text(
-                    'Hey, pal!',
-                    //style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
-                    style: GoogleFonts.dongle( textStyle:Theme.of(context).textTheme.headline1),
-                  ), Text(
-                    'You have X credits available',
-                    //style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
-                    style: GoogleFonts.roboto( textStyle:Theme.of(context).textTheme.bodyText2),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Hey, pal!',
+                              //style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+                              style: GoogleFonts.dongle(
+                                  textStyle:
+                                      Theme.of(context).textTheme.headline1),
+                            ),
+                            Text(
+                              'You have X credits available',
+                              //style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+                              style: GoogleFonts.roboto(
+                                  textStyle:
+                                      Theme.of(context).textTheme.bodyText2),
+                            ),
+                          ]),
+                      ElevatedButton(
+                        style: ButtonStyle(
+                            elevation: MaterialStateProperty.all<double>(0),
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                Color(0xffF9F9F9)),
+                            shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side: BorderSide(
+                                        width: 2.5,
+                                        color: Color(0xff95D4D8))))),
+                        onPressed: () => goToOnBoarding(context),
+                        child: Text(
+                          'Purchase Credits',
+                          style: GoogleFonts.roboto(
+                              textStyle: Theme.of(context).textTheme.button),
+                        ),
+                      ),
+                    ]),
+                const SizedBox(height: 24),
+                ElevatedButton(
+                  style: ButtonStyle(
+                      elevation: MaterialStateProperty.all<double>(0),
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          Color.fromRGBO(149, 212, 216, 1)),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              side: BorderSide(color: Color(0xff95D4D8))))),
+                  onPressed: () => goToApptPage(context),
+                  child: Text(
+                    'Schedule an Appointment',
+                    style: GoogleFonts.roboto(
+                        textStyle: Theme.of(context).textTheme.button),
                   ),
-                ]),
-                  ElevatedButton(
-                    style: ButtonStyle(
-                        elevation: MaterialStateProperty.all<double>(0),
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                            Color(0xffF9F9F9)),
-                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(18.0),
-                                side: BorderSide(
-                                    width: 2.5,
-                                    color: Color(0xff95D4D8))))),
-                    onPressed: () => goToOnBoarding(context),
-                    child: Text('Purchase Credits',
-                        style: GoogleFonts.roboto( textStyle:Theme.of(context).textTheme.button),),
-                  ),
-                ]),
-              const SizedBox(height: 24),
-              ElevatedButton(
-                style: ButtonStyle(
-                    elevation: MaterialStateProperty.all<double>(0),
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                        Color.fromRGBO(149, 212, 216, 1)),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18.0),
-                            side: BorderSide(
-                                color: Color(0xff95D4D8))))),
-                onPressed: () => goToOnBoarding(context),
-                child: Text('Schedule an Appointment', style: GoogleFonts.roboto( textStyle:Theme.of(context).textTheme.button),),
-              ),
-          Row(mainAxisAlignment: MainAxisAlignment.start,
-            children: [Column (mainAxisAlignment: MainAxisAlignment.start, crossAxisAlignment: CrossAxisAlignment.start,
-            children: [Text(
-            'Today',
-            //style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
-            style: GoogleFonts.dongle( textStyle:Theme.of(context).textTheme.headline1),
-          ), Text(
-            'No appointments scheduled today',
-            //style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
-            style: GoogleFonts.roboto( textStyle:Theme.of(context).textTheme.bodyText2),
-          ),
-              ElevatedButton( //TODO: REMOVE FOR TESTING ONLY, should replace with stateful widget
-                style: ButtonStyle(
-                    elevation: MaterialStateProperty.all<double>(0),
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                        Color.fromRGBO(149, 212, 216, 1)),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18.0),
-                            side: BorderSide(
-                                color: Color(0xff95D4D8))))),
-                onPressed: () => goToCall(context),
-                child: Text('Call', style: GoogleFonts.roboto( textStyle:Theme.of(context).textTheme.button),),
-              ),
-          ],),],
-          ),
-              Row(mainAxisAlignment: MainAxisAlignment.start,
-                children: [Column (mainAxisAlignment: MainAxisAlignment.start, crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [ Flex (direction: Axis.horizontal, mainAxisSize: MainAxisSize.max, mainAxisAlignment: MainAxisAlignment.spaceBetween, crossAxisAlignment: CrossAxisAlignment.center, children: [
-                    Text(
-                      'This Month',
-                      //style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
-                      style: GoogleFonts.dongle( textStyle:Theme.of(context).textTheme.headline1),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Today',
+                          //style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+                          style: GoogleFonts.dongle(
+                              textStyle: Theme.of(context).textTheme.headline1),
+                        ),
+                        Text(
+                          'No appointments scheduled today',
+                          //style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+                          style: GoogleFonts.roboto(
+                              textStyle: Theme.of(context).textTheme.bodyText2),
+                        ),
+                        ElevatedButton(
+                          //TODO: REMOVE FOR TESTING ONLY, should replace with stateful widget
+                          style: ButtonStyle(
+                              elevation: MaterialStateProperty.all<double>(0),
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                  Color.fromRGBO(149, 212, 216, 1)),
+                              shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(18.0),
+                                      side: BorderSide(
+                                          color: Color(0xff41434D))))),
+                          onPressed: () => goToCall(context),
+                          child: Text(
+                            'Call',
+                            style: GoogleFonts.roboto(
+                                textStyle: Theme.of(context).textTheme.button),
+                          ),
+                        ),
+                      ],
                     ),
-                    Padding(padding: EdgeInsets.fromLTRB(120, 0, 0, 0), child: const Icon(
-                      Icons.calendar_month_rounded,
-                      color:  Color(0xff41434D),
-                      size: 30.0,
-                      semanticLabel: 'Text to announce in accessibility modes',
-                    ),)
-                  ],),
-                    Text(
-                    'No upcoming appointments this month',
-                    //style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
-                    style: GoogleFonts.roboto( textStyle:Theme.of(context).textTheme.bodyText2),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Flex(
+                          direction: Axis.horizontal,
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              'This Month',
+                              //style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+                              style: GoogleFonts.dongle(
+                                  textStyle:
+                                      Theme.of(context).textTheme.headline1),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.fromLTRB(120, 0, 0, 0),
+                              child: const Icon(
+                                Icons.calendar_month_rounded,
+                                color: Color(0xff41434D),
+                                size: 30.0,
+                                semanticLabel:
+                                    'Text to announce in accessibility modes',
+                              ),
+                            )
+                          ],
+                        ),
+                        Text(
+                          'No upcoming appointments this month',
+                          //style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+                          style: GoogleFonts.roboto(
+                              textStyle: Theme.of(context).textTheme.bodyText2),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                      elevation: MaterialStateProperty.all<double>(0),
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          Color.fromRGBO(149, 212, 216, 1)),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              side: BorderSide(color: Color(0xff95D4D8))))),
+                  onPressed: () => goToOnBoarding(context),
+                  child: Text(
+                    'Go Back',
+                    style: GoogleFonts.roboto(
+                        textStyle: Theme.of(context).textTheme.button),
                   ),
-                  ],),],
-              ),
-              ElevatedButton(
-                style: ButtonStyle(
-                    elevation: MaterialStateProperty.all<double>(0),
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                        Color.fromRGBO(149, 212, 216, 1)),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18.0),
-                            side: BorderSide(
-                                color: Color(0xff95D4D8))))),
-                onPressed: () => goToOnBoarding(context),
-                child: Text('Go Back', style: GoogleFonts.roboto( textStyle:Theme.of(context).textTheme.button),),
-              ),
-            ], // Chillen
-          ),
+                ),
+              ], // Chillen
+            ),
           ),
         ),
       );
 
   void goToCall(context) => Navigator.of(context).pushReplacement(
-    MaterialPageRoute(builder: (_) => CallPage()),
-  );
+        MaterialPageRoute(builder: (_) => CallPage()),
+      );
 
   void goToOnBoarding(context) => Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: (_) => OnboardingPage()),
       );
 
   void goToResources(context) => Navigator.of(context).pushReplacement(
-    MaterialPageRoute(builder: (_) => ResourcesPage()),
-  );
+        MaterialPageRoute(builder: (_) => ResourcesPage()),
+      );
+  void goToApptPage(context) => Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (_) => ApptPage()),
+      );
 }
